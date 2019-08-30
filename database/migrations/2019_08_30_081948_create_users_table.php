b1376f4469ae6e31784d9c0b32419b31f3af97e0
Mostafa Soufi <mostafa.soufi@hotmail.com>
2019-08-30 12:56:35 +0430

    Add users table migration

---
+++ database/migrations/2019_08_30_081948_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->char('first_name', 100);
+            $table->char('last_name', 100);
+            $table->char('email')->nullable();
+            $table->char('password', 100)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
