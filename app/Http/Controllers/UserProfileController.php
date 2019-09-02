a3cba5864078c23a6cf78f76bc844b3961576d01
Mostafa Soufi <mostafa.soufi@hotmail.com>
2019-09-02 15:54:32 +0430

    Complete profile endpoints

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -2,9 +2,64 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Traits\UserProfile;
+use App\Http\Resources\ProfileResource;
+use App\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class UserProfileController extends Controller
 {
-    use UserProfile;
+    /**
+     * Show profile method.
+     *
+     * @param Request $request
+     * @return ProfileResource
+     */
+    public function show(Request $request)
+    {
+        return new ProfileResource(User::find($request->user()->id));
+    }
+
+    /**
+     * Update profile method.
+     *
+     * @param Request $request
+     * @return ProfileResource
+     * @throws ValidationException
+     */
+    public function update(Request $request)
+    {
+        $this->validate($request, [
+            'first_name' => 'required|string|max:100',
+            'last_name' => 'required|string|max:100',
+        ]);
+
+        $user = User::find($request->user()->id);
+        $user->first_name = $request->input('first_name');
+        $user->last_name = $request->input('last_name');
+
+        $user->save();
+
+        return new ProfileResource($user);
+    }
+
+    /**
+     * Update password method.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ValidationException
+     */
+    public function password(Request $request)
+    {
+        $this->validate($request, [
+            'password' => 'required|string|min:6',
+        ]);
+
+        $password = password_hash($request->input('password'), PASSWORD_DEFAULT);
+        User::where('id', $request->user()->id)->update(['password' => $password]);
+
+        return response()->json(['error' => false, 'message' => __('message.password.changed')]);
+    }
 }
--- app/Http/Resources/ProfileResource.php
+++ app/Http/Resources/ProfileResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\Resource;
 
-class Profile extends Resource
+class ProfileResource extends Resource
 {
     /**
      * Transform the resource into an array.
