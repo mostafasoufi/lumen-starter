167b4acc9c86bcf60e6106dd34c58aa4f7f7b82c
Mostafa Soufi <mostafa.soufi@hotmail.com>
2019-09-05 10:56:13 +0430

    Update packages

--- tests/UserAccountTest.php
+++ tests/UserAccountTest.php
@@ -5,28 +5,24 @@ class UserAccountTest extends TestCase
     public function testRegister()
     {
         $this->post('v1/user/register');
-
         $this->assertJson($this->response->getContent());
     }
 
     public function testVerify()
     {
         $this->post('v1/user/verify');
-
         $this->assertJson($this->response->getContent());
     }
 
     public function testForgot()
     {
         $this->post('v1/user/forgot');
-
         $this->assertJson($this->response->getContent());
     }
 
     public function testReset()
     {
         $this->post('v1/user/reset');
-
         $this->assertJson($this->response->getContent());
     }
 }
--- tests/UserProfileTest.php
+++ tests/UserProfileTest.php
@@ -5,21 +5,18 @@ class UserProfileTest extends TestCase
     public function testGetProfile()
     {
         $this->get('v1/user/profile');
-
         $this->assertJson($this->response->getContent());
     }
 
     public function testUpdateProfile()
     {
         $this->put('v1/user/profile');
-
         $this->assertJson($this->response->getContent());
     }
 
     public function testChangePassword()
     {
         $this->put('v1/user/password');
-
         $this->assertJson($this->response->getContent());
     }
 }
